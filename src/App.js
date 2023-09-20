cde7e755e2055dfb5c71aa84675835dfe762c948
Gaurav-Korpad <gauravkorpad7729@gmail.com>
2023-09-20 13:59:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Project3 from "./Project 3/Main"
 function App() {
-  return ( 
+  return (  
     <div >
      <Project3/>
     </div> 
